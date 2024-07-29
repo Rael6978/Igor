90e5bb17e33c36cc304ab6b254da65af6f793bf6
Rael caraioo <135059790+Rael6978@users.noreply.github.com>
2024-07-29 17:52:10 -0300

    Update Aluno.py

--- Aluno.py
+++ Aluno.py
@@ -1,30 +1,48 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.__nome = nome
+        self.__idade = idade
 
+    def get_nome(self):
+        return self.__nome
+    
+    def get_idade(self):
+        return self.__idade
+    def set_idade(self, NovaIdade):
+        self.__idade = NovaIdade
 
- 
-class Aluno:
-    def __init__(self, nome, turma):
-        self.nome = nome
-        self.turma = turma
-    def exibir_informacoes(self):
-       print(f"Aluno - Nome: {self.nome}, Turma: {self.turma}")
-  
+class Aluno (Pessoa):
+    def __init__(self, nome, idade, turma):
+        super().__init__(nome, idade)
+        self.__turma = turma
+        self.lista_notas = []
+        
+    def lista_notas(self):
+
+        
+
+    def get_turma(self):
+        return self.__turma
+    def set_turma(self, NovaTurma):
+        self.__turma = NovaTurma
+      
 class Disciplina(Aluno):
     def __init__(self, nome, turma, professor, nomeD):
         super().__init__(nome, turma)
-        self.professor = professor
-        self. nomeD = nomeD
+        self.__professor = professor
+        self.__nomeD = nomeD
+
+    def get_professor(self):
+        return self.__professor
+    def set_professor(self, NovoProf):
+         self.__professor = NovoProf
     
-    def exibir_informacoes(self):
-        return super().exibir_informacoes()
-        print(f"Disciplina - Nome: {self.nomeD}, Professor: {self.professor}")
-    
+
 class Nota(Disciplina):
     def __init__(self, nome, turma, professor, nota):
         super().__init__(nome, turma, professor)
-        self.nota = nota
-
-    def exibir_informacoes(self):
-        return super().exibir_informacoes()
-        print(f"Nota: {self.nota}")
-
- 
+        self.__nota = nota
+    def get_nota(self):
+        return self.__nota
+    def set_nome(self):
+        return self.__nota 
